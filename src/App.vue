596da33dd8d800eca012294b68197eab1da00dd2
dhw2040 <dhw2040@github.com>
2022-05-13 19:06:56 -0400

    weather app init

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,67 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view />
+  <router-view /> -->
+
+  <div id="app">
+    <main>
+      <SearchBox />
+      <div class="weather-box">
+        <div class="location">Toronto, Canada</div>
+        <div class="date">Fri May 13, 6:14pm</div>
+        <div class="weather">
+          <div class="icon">
+            <i class="fa-solid fa-sun"></i>
+          </div>
+          <div class="details">
+            <span class="temp">22 °C</span>
+            <hr />
+            <span class="summary"> Sunny </span>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
+<script>
+import SearchBox from "./components/SearchBox.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      api_key: "4adf2af9535e6bc15615b1fe824f621b",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+    };
+  },
+  components: { SearchBox },
+};
+</script>
+
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-image: url("./assets/sunset.png");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.3s;
 }
 
 nav {
   padding: 30px;
+  position: fixed;
+  margin: 0 auto;
 }
 
 nav a {
@@ -26,5 +71,74 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+hr {
+  margin: 1rem 0;
+  border: 0;
+  height: 0.1rem;
+  background-image: linear-gradient(
+    to right,
+    rgba(255, 255, 255, 0),
+    rgba(255, 255, 255, 0.75),
+    rgba(255, 255, 255, 0)
+  );
+}
+
+main {
+  min-height: 100vh;
+  min-width: 100vw;
+  background: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.2),
+    rgba(0, 0, 0, 0.5)
+  );
+}
+
+.weather-box {
+  color: floralwhite;
+}
+
+.weather-box .location {
+  font-size: 3rem;
+  text-shadow: 0.2rem 0.4rem rgba(0, 0, 0, 0.5);
+}
+.weather-box .date {
+  font-style: italic;
+  margin-bottom: 2rem;
+}
+.weather-box .weather {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 2rem 0 2rem 0;
+  margin: auto;
+  height: 15rem;
+  width: 40%;
+  background-color: rgba(255, 255, 255, 0.5);
+  box-shadow: 0 0 1rem 1rem rgba(255, 102, 0, 0.5);
+}
+
+.details {
+  /* font-size: 3rem; */
+  margin-left: 3rem;
+}
+
+.temp {
+  font-size: 5rem;
+  font-weight: bolder;
+  text-shadow: 0.2rem 0.4rem rgba(0, 0, 0, 0.5);
+}
+
+.summary {
+  font-size: 3rem;
+  text-shadow: 0.2rem 0.4rem rgba(0, 0, 0, 0.5);
+}
+
+/* FONT AWESOME */
+
+.fa-sun {
+  color: coral;
+  font-size: 10rem;
 }
 </style>
---
+++ src/components/SearchBox.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="search-box">
+    <input
+      class="input-field"
+      type="text"
+      name="search-box"
+      placeholder="Search your location"
+    />
+    <div class="use-location"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.search-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 30vh;
+
+  /* width: 70%; */
+  /* margin-bottom: 10px; */
+}
+
+.input-field {
+  height: 2.3rem;
+  width: 65%;
+  padding-left: 2rem;
+
+  appearance: none;
+  background: none;
+  border: none;
+  outline: none;
+
+  border-radius: 0.4rem;
+  background-color: rgba(255, 255, 255, 0.5);
+  box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.25);
+  transition: 0.3s;
+}
+
+.input-field:focus {
+  box-shadow: 0 0 0.5rem 0.1rem rgba(0, 183, 255, 0.75);
+  background-color: rgba(255, 255, 255, 0.75);
+}
+</style>
